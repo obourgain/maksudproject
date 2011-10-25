9a375dfc1766377d1878728f97c8d524d16e8703
maksud.buet@gmail.com <maksud.buet@gmail.com@d5200473-c054-0410-b44d-0ffda859ebdb>
2011-10-25 16:31:26 +0000

    /usr/include/flexsc

---
+++ trunk/flexsc/usrinclude/flexsc.h
@@ -0,0 +1,26 @@
+/*
+ * flexsc.h
+ *
+ *  Created on: Oct 14, 2011
+ *      Author: maksud
+ */
+
+#ifndef _FLEXSC_FLEXSC_H_
+#define _FLEXSC_FLEXSC_H_
+
+#include <linux/sched.h>
+
+struct syscall_entry {
+	unsigned int syscall; //4 bytes
+	unsigned char num_args; //1 byte
+	unsigned char status; //1 byte
+	long long args[6]; //48 bytes
+	long long return_code; //8 byte
+};
+
+struct syscall_page {
+	struct task_struct* threads[64];
+	struct syscall_entry entries[64];
+};
+
+#endif /* _FLEXSC_FLEXSC_H_ */
